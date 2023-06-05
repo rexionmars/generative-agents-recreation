703ff5748bacb9cc1dceea8ee5d44a00eaa7ec26
rexionmars <opensource.leonardi@gmail.com>
2023-06-05 18:21:58 -0300

    Implemented deep mind test

--- lab/neural-test-agents/config.py
+++
@@ -1,2 +0,0 @@
-CHAT_1 = "cria uma pergunta sobre programação, exemplo: Oi, como faço um for em python?"
-CHAT_2 = "Oi"
---
+++ lab/neural-test-agents/deep_mind/config.py
@@ -0,0 +1,2 @@
+CHAT_1="Criar uma mensagem de boas vindas, exemplo: Oi Leonardi, o que deseja fazer?"
+ASK_RESPONSE=""
